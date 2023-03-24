73de376c090d8d0fbd9ec83bd2b90cd7c3de173f
wjin17 <willjin1796@gmail.com>
2023-03-23 21:41:06 -0700

    re-add css import : )

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,8 @@ import { ContextProvider } from '@/hooks/useContext'
 import type { AppProps } from 'next/app'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
+import '@/styles/globals.css'
+
 const queryClient = new QueryClient()
 
 export default function App({ Component, pageProps }: AppProps) {
