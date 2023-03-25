ae4fd4b74a69306974d37532b884489613435286
wjin17 <willjin1796@gmail.com>
2023-03-24 22:30:29 -0700

    remove extra hook from root

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,4 @@
 import PrimaryLayout from '@/components/layout/Primary'
-import { ContextProvider } from '@/hooks/useContext'
 import type { AppProps } from 'next/app'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
@@ -10,11 +9,9 @@ const queryClient = new QueryClient()
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
-      <ContextProvider>
-        <PrimaryLayout>
-          <Component {...pageProps} />
-        </PrimaryLayout>
-      </ContextProvider>
+      <PrimaryLayout>
+        <Component {...pageProps} />
+      </PrimaryLayout>
     </QueryClientProvider>
   )
 }
