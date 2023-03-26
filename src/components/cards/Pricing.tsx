81f1569fe37ac28f5581f4c2b0396f0758ea0755
wjin17 <willjin1796@gmail.com>
2023-03-26 11:39:47 -0700

    remove pricing card static height

--- src/components/cards/Pricing.tsx
+++ src/components/cards/Pricing.tsx
@@ -11,11 +11,13 @@ const PricingCard: React.FC<IPricingCard> = ({
   children,
 }) => {
   return (
-    <div className="h-[28rem] flex-1 p-4">
+    <div className="flex-1 p-4">
       <div className="flex h-full w-full flex-col rounded-3xl bg-white p-8 shadow-xl">
-        <h1 className="mb-4 text-2xl">{title}</h1>
-        <p className="text-sm text-neutral-500">{description}</p>
-        <div className="font-medium">{children}</div>
+        <div>
+          <h1 className="text-2xl">{title}</h1>
+          <p className="mt-4 text-sm text-neutral-500">{description}</p>
+        </div>
+        {children}
       </div>
     </div>
   )
--- src/components/heroes/Pricing.tsx
+++ src/components/heroes/Pricing.tsx
@@ -8,7 +8,7 @@ const PricingHero: React.FC<IPricingHero> = () => {
   return (
     <section
       id="pricing"
-      className="mx-auto min-h-screen max-w-7xl items-center justify-between px-8 pt-32"
+      className="mx-auto max-w-7xl items-center justify-between px-8 pt-32"
     >
       <h1 className="text-5xl font-bold text-amber-900 md:text-7xl">Pricing</h1>
       <div className="mt-16 grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4">
@@ -16,64 +16,78 @@ const PricingHero: React.FC<IPricingHero> = () => {
           title="Open play"
           //description="Admissions is per child. Adults and siblings under 12 months are free. No reservations needed. Admissions is valid for full day access with re-entry. No restrictions on number of adults."
         >
-          <div className="flex xl:justify-between">
-            <h1 className="w-52 xl:w-auto">Little Bee {'(Age 1-10yrs)'}</h1>
-            <h1>$15</h1>
-          </div>
-          <div className="my-4 flex xl:justify-between">
-            <h1 className="w-52 xl:w-auto">Sibling {'(Age 1-10yrs)'}</h1>
-            <h1>$10</h1>
-          </div>
-          <div className="flex xl:justify-between">
-            <h1 className="w-52 xl:w-auto">Baby Bee {'(Under 1yr)'}</h1>
-            <h1>$10</h1>
+          <div className="mt-auto font-medium">
+            <div className="flex xl:justify-between">
+              <h1 className="w-52 xl:w-auto">Little Bee {'(Age 1-10yrs)'}</h1>
+              <h1>$15</h1>
+            </div>
+            <div className="mt-4 flex xl:justify-between">
+              <h1 className="w-52 xl:w-auto">Sibling {'(Age 1-10yrs)'}</h1>
+              <h1>$10</h1>
+            </div>
+            <div className="mt-4 flex xl:justify-between">
+              <h1 className="w-52 xl:w-auto">Baby Bee {'(Under 1yr)'}</h1>
+              <h1>$10</h1>
+            </div>
+            <RoundLink
+              href="/visit"
+              label="More info"
+              className="mt-4 bg-amber-300 hover:bg-amber-200"
+            />
           </div>
         </PricingCard>
         <PricingCard
           title="Private play"
           description="Private play admission is per person (adults and children) and is charged per hour. Reservations must be made in advance."
         >
-          <div className="mt-20 flex xl:justify-between">
-            <h1 className="w-52 xl:w-auto">Per Bee {'(Over 1yr)'}</h1>
-            <h1>$25</h1>
+          <div className="mt-auto font-medium">
+            <div className="mt-8 flex lg:mt-8 xl:justify-between">
+              <h1 className="w-52 xl:w-auto">Per Bee {'(Over 1yr)'}</h1>
+              <h1>$25</h1>
+            </div>
+            <div className="mt-4 flex xl:justify-between">
+              <h1 className="w-52 xl:w-auto">Baby Bee {'(Under 1yr)'}</h1>
+              <h1>$14</h1>
+            </div>
+            <RoundLink
+              href="#location"
+              label="Contact us"
+              className="mt-4 bg-amber-300 hover:bg-amber-200"
+            />
           </div>
-          <div className="mt-4 flex xl:justify-between">
-            <h1 className="w-52 xl:w-auto">Baby Bee {'(Under 1yr)'}</h1>
-            <h1>$14</h1>
-          </div>
-          <RoundLink
-            href="#location"
-            label="Contact us"
-            className="mt-4 bg-amber-300 hover:bg-amber-200"
-          />
         </PricingCard>
         <PricingCard
           title="Memberships"
           description="Members get unlimited access to open play and 20% off parties."
         >
-          <div className="mt-[7.5rem] flex xl:justify-between">
-            <h1 className="w-52 xl:w-auto">Little Bee {'(Age 6mo+)'}</h1>
-            <h1>$45</h1>
+          <div className="mt-auto font-medium">
+            <div className="mt-8 flex lg:mt-8 xl:justify-between">
+              <h1 className="w-52 xl:w-auto">Little Bee {'(Age 6mo+)'}</h1>
+              <h1>$45</h1>
+            </div>
+            <div className="mt-4 flex xl:justify-between">
+              <h1 className="w-52 xl:w-auto">Any additional bees</h1>
+              <h1>+$20</h1>
+            </div>
+
+            <RoundLink
+              href="#location"
+              label="Contact us"
+              className="mt-4 bg-amber-300 hover:bg-amber-200"
+            />
           </div>
-          <div className="mt-4 flex xl:justify-between">
-            <h1 className="w-52 xl:w-auto">Any additional bees</h1>
-            <h1>+$20</h1>
-          </div>
-          <RoundLink
-            href="#location"
-            label="Contact us"
-            className="mt-4 bg-amber-300 hover:bg-amber-200"
-          />
         </PricingCard>
         <PricingCard
           title="Parties"
-          description="Starts at $650 for 15 children and includes exclusive use of our space for 4 hours. Other packages available as well!"
+          description="Starts at $400 for 10 children and includes exclusive use of our space for 2.5 hours. Other packages available as well!"
         >
-          <RoundLink
-            label="Learn more"
-            href="/parties"
-            className="mt-[11.25rem] bg-amber-300 hover:bg-amber-200"
-          />
+          <div className="mt-auto font-medium">
+            <RoundLink
+              label="Learn more"
+              href="/parties"
+              className="mt-8 bg-amber-300 hover:bg-amber-200"
+            />
+          </div>
         </PricingCard>
       </div>
     </section>
